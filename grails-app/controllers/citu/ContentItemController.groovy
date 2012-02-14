bd4c45c87b4acfe5b9878b3ada9e2465b627db61
beanie <ben@chilling.co.uk>
2012-02-14 09:51:02 +0000

    removed crappy file upload plugin

--- grails-app/controllers/citu/ContentItemController.groovy
+++ grails-app/controllers/citu/ContentItemController.groovy
@@ -1,24 +1,9 @@
 package citu
 
-//import com.lucastex.grails.fileuploader.UFile
-
 class ContentItemController extends BaseController {
 
 	def beforeInterceptor = [action:this.&auth]
 
 	def scaffold = true
 
-    /*
-	def add = {
-		log.debug "Uploaded file with id=${params.ufileId}"
-		[files: UFile.list(), params:params]
-	}
-
-
-	def delete = {
-		def ufile = UFile.get(params.id)
-		ufile.delete()
-		redirect action:index
-	}
-	*/
 }
