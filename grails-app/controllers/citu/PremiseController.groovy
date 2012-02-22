6fd59ca57ad47985588ec80810cbb93aa5138f69
beanie <ben@chilling.co.uk>
2012-02-22 06:57:47 +0000

    Updates to high low average work

--- grails-app/controllers/citu/PremiseController.groovy
+++ grails-app/controllers/citu/PremiseController.groovy
@@ -82,46 +82,10 @@ class PremiseController extends BaseController {
 		return view
 	}
 
-	Map simpleView(Map params) {
-
-		def Premise premiseInstance = HelperUtil.getPremise(params)
-
-		DateTime dt = new DateTime()
-
-		def now = Calendar.getInstance()
-		def view
-
-		if (params.day && params.month && params.year && !params.week) {
-			log.info("simpleMapView - Day View")
-			now.set(params.int("year"), params.int("month")-1, params.int("day"))
-			view = createViewJsonObject(premiseInstance, now, "day")
-		} else if (params.day && params.month && params.year && params.week) {
-			log.info("simpleMapView - Week View")
-			now.set(params.int("year"), params.int("month")-1, params.int("day"))
-			view = createViewJsonObject(premiseInstance, now, "week")
-		} else if (params.month && params.year) {
-			log.info("simpleMapView - Month View")
-			now.set(params.int("year"), params.int("month")-1, 1, 00, 00, 00)
-			view = createViewJsonObject(premiseInstance, now, "month")
-		} else if (params.year) {
-			log.info("simpleMapView - Year View")
-			now.set(params.int("year"), 0, 1)
-			view = createViewJsonObject(premiseInstance, now, "year")
-		} else if (params.daysBack ){
-			log.info(params.daysBack +" days back view")
-			def nowDate = new Date() - params.int("daysBack")
-			now.setTime(nowDate)
-		} else {
-			log.error("Invalid date params sent")
-		}
-
-		return view
-	}
-
 	Map createViewJsonObject(Premise premiseInstance, DateTime now, String viewType) {
 
 		def user = [firstName:premiseInstance.user.firstName, lastName:premiseInstance.user.lastName, userName:premiseInstance.user.userName, contactEmail:premiseInstance.user.contactEmail, userId: premiseInstance.user.id]
-		def premise = [flatNo:premiseInstance.flatNo, addressLine1:premiseInstance.addressLine1, addressLine1:premiseInstance.addressLine2, postCode:premiseInstance.postCode, rooms:premiseInstance.rooms, squareArea:premiseInstance.squareArea, user:user]
+		def premise = [flatNo:premiseInstance.flatNo, addressLine1:premiseInstance.addressLine1, addressLine1:premiseInstance.addressLine2, postCode:premiseInstance.postCode, bedrooms:premiseInstance.bedrooms, squareArea:premiseInstance.squareArea, user:user]
 		def averages = [:]
 
 		if (viewType.equals("day"))	{
@@ -130,6 +94,7 @@ class PremiseController extends BaseController {
 
 			premiseInstance.elecReadings = ElecReading.findAllByPremiseAndFileDateBetween(premiseInstance, now.toDate(), endOfDay.toDate(), [sort:"fileDate", order:"desc"])
 			premiseInstance.waterReadings = WaterReading.findAllByPremiseAndFileDateBetween(premiseInstance, now.toDate(), endOfDay.toDate(), [sort:"fileDate", order:"desc"])
+		
 		} else if (viewType.equals("week")) {
 
 			now = now.withDayOfWeek(DateTimeConstants.MONDAY)
@@ -145,12 +110,14 @@ class PremiseController extends BaseController {
 			}
 			premiseInstance.elecReadings = electricityReadings
 			premiseInstance.waterReadings = waterReadings
+		
 		} else if (viewType.equals("month")) {
+			
 			ArrayList electricityReadings = new ArrayList()
 			ArrayList waterReadings = new ArrayList()
 			def monthDays = now.dayOfMonth().getMaximumValue()
-			//TODO reimpliment this
-			averages = getElecAverage(premiseInstance.rooms, now.toDate(), now.plusDays((now.dayOfMonth().getMaximumValue())+1).minusSeconds(1).toDate())
+			averages = getTrueAverage(premiseInstance.bedrooms, now.toDate(), now.plusMonths(1).minusSeconds(1).toDate())
+			//getMaxMinAverage(premiseInstance.bedrooms, now.toDate(), now.plusMonths(1).minusSeconds(1).toDate())
 			4.times {
 				def endOfWeek = now.plusDays(7).minusSeconds(1)
 				def elecWeek = ElecReading.findAllByPremiseAndFileDateBetween(premiseInstance, now.toDate(), endOfWeek.toDate(), [sort:"fileDate", order:"desc"])
@@ -193,52 +160,63 @@ class PremiseController extends BaseController {
 		return premise
 	}
 
-	Map getElecAverage(int noOfRooms, Date startDate, Date endDate) {
-		def premises = Premise.executeQuery("select p.flatNo from Premise p where rooms = "+ noOfRooms)
+	Map getTrueAverage(int noOfRooms, Date startDate, Date endDate) {
+		def premises = Premise.executeQuery("select p.flatNo from Premise p where bedrooms = "+ noOfRooms)
 		def averages = [:]
 		def tmpElecFloat = 0
 		def tmpHeatFloat = 0
 		def tmpGreyFloat = 0
 		def tmpColdFloat = 0
 		def tmpHotFloat = 0
-		def goodElecReadings = 0
-		def goodHeatReadings = 0
-		def goodWaterReadings = 0
+		def heatReadings = new ArrayList()
+		def waterHotReadings = new ArrayList()
+		def waterColdReadings = new ArrayList()
+		def waterGreyReadings = new ArrayList()
+		def elecReadings = new ArrayList()
 		for (i in premises) {
 			def sumElec = ElecReading.executeQuery("select sum(reading.readingValueElec) from ElecReading as reading where reading.premise.flatNo = "+ i +" and reading.fileDate between:date1 AND :date2 ", [date1:startDate, date2:endDate])
 			def sumHeat = HeatReading.executeQuery("select sum(reading.readingValueHeat) from HeatReading as reading where reading.premise.flatNo = "+ i +" and reading.dateCreated between:date1 AND :date2 ", [date1:startDate, date2:endDate])
 			def sumWater = WaterReading.executeQuery("select sum(reading.readingValueHot), sum(reading.readingValueCold), sum(reading.readingValueGrey) from WaterReading as reading where reading.premise.flatNo = "+ i +" and reading.fileDate between:date1 AND :date2 ", [date1:startDate, date2:endDate])
-			// ignore Elec for empty values
+			// ignore Elec for empty values			
 			if (sumElec[0]) {
-				log.info(startDate)
-				log.info(endDate)
-				log.info("flat "+i + " " +sumElec)
 				tmpElecFloat = (tmpElecFloat + sumElec[0])
-				goodElecReadings ++
+				elecReadings.add(sumElec[0])
 			}
 			// ignore Heat for empty values
 			if (sumHeat[0]) {
 				tmpHeatFloat = (tmpHeatFloat + sumHeat[0])
-				goodHeatReadings ++
+				heatReadings.add(sumHeat[0])
 			}
 			// ignore Water for empty values
 			if (sumWater[0][2]) {
 				tmpHotFloat = (tmpHotFloat + sumWater[0][0])
 				tmpColdFloat = (tmpColdFloat + sumWater[0][1])
 				tmpGreyFloat = (tmpGreyFloat + sumWater[0][2])
-				goodWaterReadings ++
+				waterHotReadings.add(sumWater[0][0])
+				waterColdReadings.add(sumWater[0][1])
+				waterGreyReadings.add(sumWater[0][2])
 			}
 		}
-		if (goodElecReadings > 0) {
-			averages.put("averageElec", (tmpElecFloat / goodElecReadings))
+		if (elecReadings.size() > 0) {
+			elecReadings.sort()
+			log.info("ELEC LOW : "+ elecReadings[0])
+			log.info("ELEC HIGH : "+ elecReadings[elecReadings.size() - 1])
+			averages.put("averageElec", (tmpElecFloat / elecReadings.size()))
 		}
-		if (goodHeatReadings > 0) {
-			averages.put("averageHeat", (tmpHeatFloat / goodHeatReadings))
+		if (heatReadings.size() > 0) {
+			heatReadings.sort()
+			log.info("HEAT LOW : "+ heatReadings[0])
+			log.info("HEAT HIGH : "+ heatReadings[heatReadings.size() - 1])
+			averages.put("averageHeat", (tmpHeatFloat / heatReadings.size()))
 		}
-		if (goodWaterReadings > 0) {
-			averages.put("averageWaterHot", (tmpHotFloat / goodWaterReadings))
-			averages.put("averageColdHot", (tmpColdFloat / goodWaterReadings))
-			averages.put("averageGreyHot", (tmpGreyFloat / goodWaterReadings))
+		if (waterGreyReadings.size() > 0) {
+			// TODO not finished the water readings sort for high and low
+			waterHotReadings.sort()
+			waterHotReadings.sort()
+			waterHotReadings.sort()
+			averages.put("averageWaterHot", (tmpHotFloat / waterHotReadings.size()))
+			averages.put("averageColdHot", (tmpColdFloat / waterColdReadings.size()))
+			averages.put("averageGreyHot", (tmpGreyFloat / waterGreyReadings.size()))
 		}
 		return averages
 	}
--- grails-app/domain/citu/Premise.groovy
+++ grails-app/domain/citu/Premise.groovy
@@ -1,30 +1,34 @@
 package citu
 
 class Premise {
-	
+
+    // TODO extend a premise to flat / office etc.
 	String flatNo
 	String addressLine1
 	String addressLine2
 	String postCode
-	int rooms
+	String core
+	String premiseType
+	int bedrooms
+	int bathrooms
 	int squareArea
-	
+
 	ArrayList elecReadings = new ArrayList()
 	ArrayList waterReadings = new ArrayList()
 	ArrayList heatReadings = new ArrayList()
-	
+
 	User user
-	
+
 	String toString() {
 		return flatNo
 	}
-	
+
 	static hasMany=[setTobBoxes:SetTopBox]
-	
+
     static constraints = {
 		addressLine1(blank:false, nullable: false)
 		addressLine2(blank:false, nullable: false)
     }
-	
+
 	static transients = ['elecReadings', 'waterReadings', 'heatReadings']
 }
