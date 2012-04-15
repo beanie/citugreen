345709938f4bf12b15bc9409b6161d7f1dff0451
phil Sellick <phil.sellick@virginmedia.co.uk>
2012-04-15 17:26:14 +0100

    Now with arrivals as well

--- grails-app/controllers/citu/TrainController.groovy
+++ grails-app/controllers/citu/TrainController.groovy
@@ -4,13 +4,10 @@ import grails.converters.*
 
 class TrainController {
 
-    def  getTrainTimes = {
-		
-			
+    def  getTrainDepartureTimes = {
+				
 		def slurper = new XmlSlurper(new org.ccil.cowan.tagsoup.Parser())
-		
 		def url = new URL("http://www.eastcoast.co.uk/travel-information/live-train-times/leeds-departures/")
-		
 		def timeTable = new ArrayList()
 		def map
 		
@@ -27,22 +24,50 @@ class TrainController {
 			}
 			def destinations = html.'**'.findAll{ it.@class == 'stationname'
 			}
-		
-	
+
 			def x = 0
 			for ( i in trainStatus ) {
 				destination = destinations[x].toString()
-				trainDeparture = trainDepartures[x].toString()
-				
-				println (" Trains "+trainDeparture)
-				println (" Destination "+destination)
-				
-				map = [Destination:destination, TrainDepartures:trainDeparture]
+				trainDeparture = trainDepartures[x].toString()	
+				map = [destination:destination, trainDeparture:trainDeparture]
 				timeTable.add(map)
 				x++
 			}
 		}
-		
 		render timeTable as JSON
-		}
 	}
+
+def  getTrainArrivalTimes = {
+	
+	def slurper = new XmlSlurper(new org.ccil.cowan.tagsoup.Parser())
+	def url = new URL("http://www.eastcoast.co.uk/travel-information/live-train-times/leeds-arrivals/")
+	def timeTable = new ArrayList()
+	def map
+
+	url.withReader { reader ->
+
+		def html = slurper.parse(reader)
+
+			String origin = ""
+			String trainArrival = ""
+
+			def trainStatus = html.'**'.findAll{ it.@class == 'trainstatus'
+			}
+			def trainArrivals = html.'**'.findAll{ it.@class == 'servicetime'
+			}
+			def origins = html.'**'.findAll{ it.@class == 'stationname'
+			}
+
+			def x = 0
+			for ( i in trainStatus ) {
+				origin  = origins[x].toString()
+				trainArrival = trainArrivals[x].toString()
+				map = [origin:origin, trainArrival:trainArrival]
+				timeTable.add(map)
+				x++
+				}
+		}
+	render timeTable as JSON
+	}
+}
+
