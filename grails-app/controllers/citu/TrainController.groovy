2347236bc96f8582cc14a4ff38cf23a942903cd3
phil Sellick <phil.sellick@virginmedia.co.uk>
2012-04-15 16:36:15 +0100

    Get in you mother - Train Depatures stage one !

---
+++ grails-app/controllers/citu/TrainController.groovy
@@ -0,0 +1,48 @@
+package citu
+import grails.converters.*
+
+
+class TrainController {
+
+    def  getTrainTimes = {
+		
+			
+		def slurper = new XmlSlurper(new org.ccil.cowan.tagsoup.Parser())
+		
+		def url = new URL("http://www.eastcoast.co.uk/travel-information/live-train-times/leeds-departures/")
+		
+		def timeTable = new ArrayList()
+		def map
+		
+		url.withReader { reader ->
+			
+			def html = slurper.parse(reader)
+			
+			String destination = ""
+			String trainDeparture = ""
+
+			def trainStatus = html.'**'.findAll{ it.@class == 'trainstatus'
+			}
+			def trainDepartures = html.'**'.findAll{ it.@class == 'servicetime'
+			}
+			def destinations = html.'**'.findAll{ it.@class == 'stationname'
+			}
+		
+	
+			def x = 0
+			for ( i in trainStatus ) {
+				destination = destinations[x].toString()
+				trainDeparture = trainDepartures[x].toString()
+				
+				println (" Trains "+trainDeparture)
+				println (" Destination "+destination)
+				
+				map = [Destination:destination, TrainDepartures:trainDeparture]
+				timeTable.add(map)
+				x++
+			}
+		}
+		
+		render timeTable as JSON
+		}
+	}
