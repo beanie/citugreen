0d7aceadf37761ba914fa2e2b9b1e23af8273837
phil sellick <phil.sellick@virginmedia.co.uk>
2012-05-03 11:58:20 +0100

    Committing the train controller for Ben to See

--- grails-app/controllers/citu/TrainController.groovy
+++ grails-app/controllers/citu/TrainController.groovy
@@ -2,38 +2,50 @@ package citu
 import grails.converters.*
 
 
+@Grab(group='org.ccil.cowan.tagsoup', module='tagsoup', version='1.2' )
+
+
 class TrainController {
 
     def  getTrainDepartureTimes = {
 				
-		def slurper = new XmlSlurper(new org.ccil.cowan.tagsoup.Parser())
-		def url = new URL("http://www.eastcoast.co.uk/travel-information/live-train-times/leeds-departures/")
+		def tagSoupParser = new org.ccil.cowan.tagsoup.Parser()
+		def slurper = new XmlSlurper(tagSoupParser)
 		def timeTable = new ArrayList()
 		def map
 		
-		url.withReader { reader ->
-			
-			def html = slurper.parse(reader)
+		def htmlParser = slurper.parse("http://www.eastcoast.co.uk/travel-information/live-train-times/leeds-departures/")
 			
 			String destination = ""
 			String trainDeparture = ""
+			
+			def timeList = []
+			def trainList = []
+			 
+		
+			htmlParser.'**'.findAll{ it.@class == 'trainstatus'
+			}.each {
+				
+				timeList.add(htmlParser.'**'.find{ it.@class == 'servicetime'
+				})			
+				trainList.add(htmlParser.'**'.find{ it.@class == 'stationname'
+				})
+			
+				println timeList[5]
+				println trainList[5]
+			}
+			
+			
 
-			def trainStatus = html.'**'.findAll{ it.@class == 'trainstatus'
-			}
-			def trainDepartures = html.'**'.findAll{ it.@class == 'servicetime'
-			}
-			def destinations = html.'**'.findAll{ it.@class == 'stationname'
-			}
-
-			def x = 0
-			for ( i in trainStatus ) {
-				destination = destinations[x].toString()
-				trainDeparture = trainDepartures[x].toString()	
-				map = [destination:destination, trainDeparture:trainDeparture]
-				timeTable.add(map)
-				x++
-			}
-		}
+	//		def x = 0
+	//		for ( i in trainStatus ) {
+			//	destination = mainDestinations[x].toString()
+			//	trainDeparture = trainDepartures[x].toString()	
+			//	map = [destination:destination, trainDeparture:trainDeparture]
+			//	timeTable.add(map)
+			//	x++
+		//	}
+		
 		render timeTable as JSON
 	}
 
