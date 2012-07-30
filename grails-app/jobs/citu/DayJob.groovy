658b8fe8d030453383891ab954ad9be3c8cd51a5
phil sellick <phil.sellick@virginmedia.co.uk>
2012-07-30 09:31:50 +0100

    Added the heating gathering as a job

--- grails-app/jobs/citu/DayJob.groovy
+++ grails-app/jobs/citu/DayJob.groovy
@@ -5,10 +5,23 @@ class DayJob {
 
 	RankService rankService
 
-	def cronExpression = "0 10 23 * * ?" // run on 0 second 0 minute all the time
+	def cronExpression = "0 10 23 ? * WED" // run on 0 second 0 minute all the time
 
     def execute() {
         log.info("starting Day quartz job")
 		rankService.generateRankings()
     }
 }
+
+class HeatReadingsJob {
+	
+		EnergyReadingService energyReadingService
+	
+		def cronExpression = "0 10 8 * * *" // run on 0 second 0 minute all the time
+	
+		def execute() {
+			log.info("starting Heat reading download")
+			energyReadingService.cleanUpHeatFiles()
+		}
+	}
+	
