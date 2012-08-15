e824c7d6399e4abea0643768fa885075501dc3e7
phil sellick <phil.sellick@virginmedia.co.uk>
2012-08-15 10:44:20 +0100

    Lastest Live deployment 15-8

--- grails-app/jobs/citu/DayJob.groovy
+++ grails-app/jobs/citu/DayJob.groovy
@@ -1,17 +1,14 @@
 package citu
 
-
 class DayJob {
 
-class HeatReadingsJob {
-	
 		EnergyReadingService energyReadingService
 		
-		def cronExpression = "0 10 8 * * *" // run on 0 second 10 minute once a day at 8 am
+		def cronExpression = "0 10 8 1/1 * ? *" // run on 0 second 10 minute once a day at 8 am
 	
 		def execute() {
 			log.info("starting Heat reading download")
 			energyReadingService.processHeat()
-		}
+
 	}
 }
--- grails-app/jobs/citu/WeekJob.groovy
+++ grails-app/jobs/citu/WeekJob.groovy
@@ -4,10 +4,10 @@ class WeekJob {
 
 	RankService rankService
 
-	def cronExpression = "0 10 23 ? * WED" // run on 0 second 0 minute all the time
+	def cronExpression = "0 0 23 ? * SUN *" // run on 0 second 0 minute all the time
 
     def execute() {
-        log.info("starting Day quartz job")
+        log.info("starting Week quartz job")
 		rankService.generateRankings()
     }
 }
